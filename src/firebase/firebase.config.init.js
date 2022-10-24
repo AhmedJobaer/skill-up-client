f6e4bc3e3890ebff46ef44c087c25984e4f170b1
jobaer-ahmed <ajobaer08@gmail.com>
2022-10-25 02:15:08 +0800

    cliet setup

---
+++ src/firebase/firebase.config.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDtVb82C2UaIlQ165CCeUMO66WUX3uI9ck",
+    authDomain: "skill-up-a0055.firebaseapp.com",
+    projectId: "skill-up-a0055",
+    storageBucket: "skill-up-a0055.appspot.com",
+    messagingSenderId: "689011117148",
+    appId: "1:689011117148:web:e678bbaac28795279c6a99"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
