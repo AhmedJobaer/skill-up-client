88cbd2fad6f3946748b6fef36e415645e2d06662
jobaer-ahmed <ajobaer08@gmail.com>
2022-10-27 17:36:58 +0800

    course detail page edit

--- src/Pages/Container/CourseDetails/CourseDetails.js
+++ src/Pages/Container/CourseDetails/CourseDetails.js
@@ -1,14 +1,37 @@
 import React from 'react';
-
+import { Image } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import { useLoaderData } from 'react-router-dom';
+
+
+
 
 const CourseDetails = () => {
     const course = useLoaderData();
     console.log(course);
     return (
-        <div className='w-50 mx-auto'>
-            <h2>{course.name}</h2>
-            <p>{course.courseDescription}</p>
+        <div>
+            <div className='w-50 mx-auto my-4'>
+
+                <Button className='' variant="success">Download Details as PDF</Button>
+                <Button variant="success">Get Premium Access</Button>
+
+
+            </div>
+            <div className='w-75  mx-auto d-flex'>
+                <Image style={{ width: '180px', height: "140px", marginRight: '15px' }} src={course.img}>
+
+                </Image>
+                <div className=''>
+                    <h2>{course.name}</h2>
+                    <p>{course.courseDescription}</p>
+                    <p>Duration: {course.duration}</p>
+                    <p>Ratings: {course.ratings}</p>
+                    <p>Review: {course.review}</p>
+                    <p>Instructor Name: {course.instructor.instructorName}</p>
+                    <p>Price: ${course.price}</p>
+                </div>
+            </div>
         </div>
     );
 };
