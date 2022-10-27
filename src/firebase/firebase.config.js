93add53dbfd60eb4d80e690b6da5c6e23c167d1f
jobaer-ahmed <ajobaer08@gmail.com>
2022-10-27 20:33:40 +0800

    emviroment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,22 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+};
+
+/* const firebaseConfig = {
     apiKey: "AIzaSyDtVb82C2UaIlQ165CCeUMO66WUX3uI9ck",
     authDomain: "skill-up-a0055.firebaseapp.com",
     projectId: "skill-up-a0055",
     storageBucket: "skill-up-a0055.appspot.com",
     messagingSenderId: "689011117148",
     appId: "1:689011117148:web:e678bbaac28795279c6a99"
-};
+}; */
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
